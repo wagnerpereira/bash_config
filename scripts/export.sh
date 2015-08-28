66beb38fc0d012d5d27971127a0afaa0b82803f5
Jhonathas Matos <jhonathas@gmail.com>
2015-08-28 15:27:58 -0300

    Export docker vars

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,4 +1,5 @@
 PATH=/usr/local/heroku/bin:$PATH
+
 export PATH=$PATH
 
 export EDITOR="nano"
@@ -18,3 +19,8 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;44;33m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[00;32m'
+
+
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=~/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
